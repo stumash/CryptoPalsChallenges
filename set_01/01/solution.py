0cab7d1bcd59265fdbc3d0e8deacc00b9f78dbaa
Stuart Mashaal <stuart.mashaal@gmail.com>
2018-06-13 11:20:06 -0400

    initial commit

---
+++ set_01/01/solution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('input_file',
+    help='''contains one line, a string of hex data''')
+args = arg_parser.parse_args()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

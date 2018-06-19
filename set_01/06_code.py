735a6ed8aef29c545d413b76b2b15ca4c12fedac
Stuart Mashaal <stuart.mashaal@gmail.com>
2018-06-19 14:44:18 -0400

    'start' set_01/06

---
+++ set_01/06_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('input_file',
+        help='''x''')
+args = arg_parser.parse_args()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

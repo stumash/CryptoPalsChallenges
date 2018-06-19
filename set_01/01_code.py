e395f577db8461f3177d442aa9c2225635993b48
stumash <stuart.mashaal@gmail.com>
2018-06-19 03:04:12 -0400

    add README.md

--- set_01/01_code.py
+++ set_01/01_code.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
 
-import argparse
-from collections import ChainMap
 from base64 import b64encode
 
+import argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('input_file',
     help='''contains one line, a string of hex data''')
 args = arg_parser.parse_args()
 
 def main():
-    hex_str = get_input_str(args.input_file)
+    with open(args.input_file, 'r') as f:
+        bts = bytes.fromhex(next(line.strip() for line in f))
 
-    # every two hex digits in the str become a single byte
-    hex_bytes = bytes.fromhex(hex_str)
-
-    # every 3 bytes converts to 4 bytes according to base64
-    b64_enc_bytes = b64encode(hex_bytes)
-
-    print(b64_enc_bytes.decode('utf-8'))
-
-def get_input_str(filename):
-    with open(filename, 'r') as f:
-        return next(line.strip() for line in f)
+    print(b64encode(bts).decode('utf-8'))
 
 if __name__ == "__main__":
     main()
---
+++ set_01/README.md
@@ -0,0 +1,10 @@
+Each `.py` in this folder can be run with
+
+`$ ./XX_code.py data/XX_input.txt`
+
+where `XX` is in `0[12345678]`.
+
+
+Where appliclable, output can be compared against the output of
+
+`$ cat data/XX_expected_output.txt`.

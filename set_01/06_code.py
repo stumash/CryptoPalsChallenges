caf5963d50a23c7f54ff6be94252f56b7c91b5d9
Stuart Mashaal <stuart.mashaal@gmail.com>
2018-06-20 18:05:22 -0400

    wip set01/06

--- set_01/06_code.py
+++ set_01/06_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from base64 import b64decode
 from common_set01 import hamming
 
 import argparse
@@ -10,7 +11,17 @@ arg_parser.add_argument('input_file',
 args = arg_parser.parse_args()
 
 def main():
-    pass
+    with open(args.input_file, 'r') as f:
+        bts = b64decode(''.join(line.strip() for line in f))
+
+    def normalized_hamming(keysize: int):
+        s = ''.join(chr(b) for b in bts[:keysize*2])
+        s1, s2 = s[:keysize], s[keysize:keysize*2]
+        return hamming(s1, s2) / keysize
+
+    probable_keysizes = sorted(range(2,40), key=normalized_hamming)[:3]
+
+    print(probable_keysizes)
 
 if __name__ == "__main__":
     main()

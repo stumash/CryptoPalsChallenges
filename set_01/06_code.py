a182591515b186a02a31abaf72ff16e172dfbf54
stumash <stuart.mashaal@gmail.com>
2018-06-28 00:09:52 -0400

    complete set01/06

--- set_01/06_code.py
+++ set_01/06_code.py
@@ -2,11 +2,12 @@
 
 from base64 import b64decode
 from common_set01 import hamming, eng_score, xor_decrypt, repeat_xor, chrify
+from statistics import mean
 
 import argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('input_file',
-        help='''file containing large b64-encoded ciphertext resulting from
+        help='''file containing base64-encoded ciphertext produced by
         repeating-xor''')
 args = arg_parser.parse_args()
 
@@ -16,16 +17,19 @@ def main():
 
     keysize = min(range(2,40), key=lambda k: normalized_hamming(bts, k))
 
-    tk      = [bts[start::keysize] for start in range(keysize)] # tk = _t_ranspose by _k_eysize
+    tk = [bts[start::keysize] for start in range(keysize)] # tk= t_ranspose by k_eysize
 
-    key     = [max(range(256), key=lambda k: eng_score(xor_decrypt(bts, k))) for bts in tk]
+    key = [max(range(256), key=lambda k: eng_score(xor_decrypt(bts, k))) for bts in tk]
 
-    dec     = repeat_xor(bts, key)
+    dec = repeat_xor(bts, key)
 
-    print('key: {}, msg: {}'.format(*map(chrify, [key,dec])))
+    print('keysize:\n{}'.format(keysize))
+    print('key:\n{}'.format(chrify(key)))
+    print('dec:\n{}'.format(chrify(dec)))
 
 def normalized_hamming(bts: bytes, keysize: int):
-    return hamming(bts[:keysize], bts[keysize:keysize*2]) / keysize
+    chunks = [bts[keysize*i:keysize*(i+1)] for i in range((len(bts)//keysize)-1)]
+    return mean(hamming(chunks[i],chunks[i+1])/keysize for i in range(len(chunks)-1))
 
 if __name__ == "__main__":
     main()

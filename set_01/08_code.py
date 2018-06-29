8267cfdbe812ce9bc8f6144b48074541fb155a0e
Stuart Mashaal <stuart.mashaal@gmail.com>
2018-06-29 14:18:40 -0400

    complete set01/08

--- set_01/08_code.py
+++ set_01/08_code.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
 import argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('input_file',
         help='''file containing lines of hex-encoded ciphertext, one
-        of which was produced by AES encryption in ECB mode''')
+        of which was produced by AES encryption in ECB mode (16-bit key)''')
 args = arg_parser.parse_args()
 
 def main():
@@ -13,10 +15,17 @@ def main():
 
     best_i,best_bts = max(enumerate(btss), key=lambda t: ecb_detect(t[1]))
 
-    print(best_i,best_bts)
+    print("line number: {}\ndata: {}".format(best_i+1,best_bts.hex()))
 
 def ecb_detect(bts):
-    return True
+    blocks = [bts[i:i+16] for i in range(0,len(bts)-16,16)]
+    cnt = Counter(blocks)
+
+    if len(blocks) != len(cnt):
+        s = "repeated 16 bytes: {}\nnumber of repetitions: {}"
+        print(next(s.format(k.hex(),cnt[k]) for k in cnt if cnt[k] > 1))
+
+    return len(blocks) != len(cnt)
 
 if __name__ == "__main__":
     main()

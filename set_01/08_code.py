f83ad6a56a8aff074012a2f8a11569fbd1bae2ed
stumash <stuart.mashaal@gmail.com>
2018-07-02 19:40:52 -0400

    clean up set01/08

--- set_01/08_code.py
+++ set_01/08_code.py
@@ -6,19 +6,27 @@ def main():
     with open('data/08_input.txt', 'r') as f:
         btss = [bytes.fromhex(line.strip()) for line in f]
 
-    best_i,best_bts = max(enumerate(btss), key=lambda t: ecb_detect(t[1]))
+    i,bts = max(enumerate(btss), key=unpackargs(lambda i,bts: ecb_detect(bts)))
 
-    print("line number: {}\ndata: {}".format(best_i+1,best_bts.hex()))
-
-def ecb_detect(bts):
     blocks = [bts[i:i+16] for i in range(0,len(bts)-16,16)]
     cnt = Counter(blocks)
 
-    if len(blocks) != len(cnt):
-        s = "repeated 16 bytes: {}\nnumber of repetitions: {}"
-        print(next(s.format(k.hex(),cnt[k]) for k in cnt if cnt[k] > 1))
+    repeated,n_repeats = next((k,cnt[k]) for k in cnt if cnt[k] > 1)
 
-    return len(blocks) != len(cnt)
+    s = (
+        'line number:           {}\n'
+        'data:                \n{}\n'
+        'repeated 16-bytes:     {}\n'
+        'number of repetitions: {}'
+    )
+    print(s.format(i+1,bts.hex(),repeated.hex(),n_repeats))
+
+def ecb_detect(bts):
+    blocks = [bts[i:i+16] for i in range(0,len(bts)-16,16)]
+    return len(blocks) != len(set(blocks))
+
+def unpackargs(f):
+    return lambda x: f(*x)
 
 if __name__ == "__main__":
     main()
